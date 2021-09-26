db059081d11c87694cae20bacc8de32c1c7dc659
よこやま～ん <19930401b>
2021-09-26 21:18:40 +0900

    var→letに修正　ソースの体裁を整理。各列の値をcookieに保存する処理を追加

--- calc.js
+++ calc.js
@@ -1,18 +1,18 @@
 (function() {
   'use strict';
 
-  var max = [];          //朝の最大血圧
-  var min = [];          //朝の最低血圧   
-  var pulse = [];        //脈拍
-  var nightmax = [];     //夜の最大血圧
-  var nightmin = [];     //夜の最低血圧
-  var nightpulse = [];   //夜の脈拍
-  var lastmax = 0;
-  var lastmin = 0;
-  var lastpulse = 0;
-  var lastnightmax = 0;
-  var lastnightmin = 0;
-  var lastpulse2 = 0;
+  let max = [];          //朝の最大血圧
+  let min = [];          //朝の最低血圧   
+  let pulse = [];        //脈拍
+  let nightmax = [];     //夜の最大血圧
+  let nightmin = [];     //夜の最低血圧
+  let nightpulse = [];   //夜の脈拍
+  let lastmax = 0;
+  let lastmin = 0;
+  let lastpulse = 0;
+  let lastnightmax = 0;
+  let lastnightmin = 0;
+  let lastpulse2 = 0;
 
   // HTML要素を変数に格納
   // getElementsByClassName()は配列要素になるので扱い注意
@@ -32,6 +32,7 @@
   /*-----------------------------------------------------------------------------------*/
   // 「平均を出力」ボタンをクリックしたときの処理
   btn.onclick = function(){
+
     // 各列の平均値を計算する
     maxmax(max,"lastmax");
     maxmax(min,"lastmin");
@@ -46,18 +47,18 @@
   // output: 平均値を出力するエディットボックス名(id名)
   function maxmax(data, output){
 
-    var num = 0;    // 正常入力データ数
-    var sum = 0;    // 合計
-    var answer = 0; // 平均値
+    let num = 0;    // 正常入力データ数
+    let sum = 0;    // 合計
+    let answer = 0; // 平均値
 
     // 要素数の分だけ繰り返す
-    for(var i = 0; i < data.length; i++){
+    for(let i = 0; i < data.length; i++){
       // 入力したデータが数字かつ、空白ではないなら計算する
       // ①isNaNで非数かどうか確認
       // ②isNaNは空白だとfalseを返すので、入力した文字列の長さで空白かどうかチェックする
       if(!isNaN(data[i].value) && data[i].value.length > 0 ){
         // ここまでくるなら数値が入力されていると判断できる
-        // 合計値に加算する
+
         sum += (parseInt(data[i].value)); // valueで入力した値を取ってる parseIntは文字列を数値に変換するのだ！！
 
         // 入力したデータ数として加算
@@ -74,7 +75,7 @@
 
   /*-------------------------------------------------------------------*/
   // 日付関連
-  var daymax = [];  // 日付情報格納先
+  let daymax = [];  // 日付情報格納先
 
   // 日付情報のエディットボックスのHTML要素を取得
   // getElementsByClassName()は配列要素になるので扱い注意
@@ -89,7 +90,7 @@
 
   /*-------------------------------------------------------------------*/
   // 日付自動入力処理
-  function daymaxx()       // ☆日にちを自動出力する関数　　　　　　　　　関数前にはfunction記載しておくことエラーでる
+  function daymaxx()       // ☆日にちを自動出力する関数　　　関数前にはfunction記載しておかないとエラーがでる
   {
     // 処理概要
     // ①ボタンを押した時点の日付情報を一番左上のエディットボックスに設定
@@ -98,29 +99,28 @@
 
     // ①ボタンを押した時点の日付情報を一番左上のエディットボックスに設定
     // ボタンを押した時点の情報を取得
-    var today  = new Date();          // ここではミリ秒で値を取得
-    var month  = today.getMonth()+1;  // 現在の月の数字を取得＋1は0から開始のため
-    var date   = today.getDate();     // 現在の日を取得ここは＋１いらない
+    let today  = new Date();          // ここではミリ秒で値を取得
+    let month  = today.getMonth()+1;  // 現在の月の数字を取得＋1は0から開始のため
+    let date   = today.getDate();     // 現在の日を取得ここは＋１いらない
   
     // 一番左上のエディットボックスを更新
     daymax[0].value =  month + "/" + date;  // ここではmonthとdateは値　"/" を入れ＋することで文字列を合体さしている。
-    // ①終わり
 
     // ②①の情報をcookieに保存
     // cookie名はname
-    document.cookie = `name=${daymax[0].value};max-age = 3000` // cookieに値を保存しcookieの寿命をmax-ageで設定  バッククォート使用 シフト＋＠  
+    document.cookie = `name=${daymax[0].value};max-age=43200` // cookieに値を保存しcookieの寿命をmax-ageで設定  バッククォート使用 シフト＋＠  
     // ②終わり
     
     // ③1日ずつ進めながら、同じ列のエディットボックスをすべて埋めていく
     // daymax[0]は一番左上なので、iの初期値は1
-    for(var i = 1; i < daymax.length; i++ )
+    for(let i = 1; i < daymax.length; i++ )
     {
       // 日付情報を1日進める
       today.setDate(today.getDate() + 1);        //  これはtodayに１足している　　これをそのまま代入するとミリ秒単位ででる。
 
       // エディットボックスに入れる情報を取得
-      month           = today.getMonth()+1;      // 月
-      date            = today.getDate();         // 日
+      month = today.getMonth()+1;      // 月
+       date = today.getDate();         // 日
 
       // エディットボックス更新
       daymax[i].value = month + "/" + date;
@@ -130,9 +130,8 @@
   /*-------------------------------------------------------------------*/
 
   // cookie関係
-  var cookiebox;                                                          // クッキーの値取得する変数 文字列である
-  var cookieabc = [];
-
+  let cookiebox;                                                          // クッキーの値取得する変数 文字列である
+  let cookieabc = [];
 
   // cookie処理
   // cookieはセミコロン(;)で区切られている。
@@ -149,24 +148,27 @@
   // cookieabc[1]: データ
 
 //splitは配列を返す。;で区切る
-  var cookiesplit = cookiebox.split(';');
+  let cookiesplit = cookiebox.split(';');
 
   //このforではcookiesplitを=で分けてnameがある箇所を探して日にちを入れる。
-  for(var i  = 0; i < cookiesplit.length; i++)
+  for(let i  = 0; i < cookiesplit.length; i++)
   {
     cookieabc = cookiesplit[i].split('=');
    // 3 左上のエディットボックスに反映  cookieの値を反映する
 
     if((cookieabc[0] === "name") || (cookieabc[0] === " name" )){
        daymax[0].value = cookieabc[1];
-       dayget();
+       dayget(); // 日付情報更新 
+
     }
 
     // cookieデータの呼び出し血圧場所
     else if((cookieabc[0] === "cookie") || (cookieabc[0] === " cookie" )){
 
+      //,で値を分割する。
       let cookiecut = cookieabc[1].split(",");
 
+//ここで各列に分割した値を入れる
       for(let j = 0; j < max.length; j++){
 
         max[j].value = cookiecut[6*j];
@@ -175,9 +177,18 @@
         nightmax[j].value = cookiecut[6*j+3];
         nightmin[j].value = cookiecut[6*j+4];
         nightpulse[j].value = cookiecut[6*j+5];
+        //onchangeが発生されない　＝でつないでいると。
 
       }
+      //値入れた後に　１平均出す関数読んで　２cookieに記憶させる。
+      maxmax(max,"lastmax");
+      maxmax(min,"lastmin");
+      maxmax(pulse,"lastpulse"); 
+      maxmax(nightmax,"lastnightmax");
+      maxmax(nightmin,"lastnightmin");
+      maxmax(nightpulse,"lastpulse2");
 
+      cookiecool();  // 血圧・心拍数データをcookieに保存する
     }
 
   }
@@ -189,9 +200,8 @@
     
     document.cookie = `name=${daymax[0].value}`;  // cookieに値を保存     バッククォート使用 シフト＋＠  
 
-    dayget();                                     // 関数呼ぶ
-    
-    cookiecool(); 
+    dayget();        // 日付情報更新                             
+    cookiecool();   // 血圧・心拍数データをcookieに保存する
   }
 
   /*------------------------------------------------------------------------------------*/
@@ -205,7 +215,7 @@
 
     // 左上の日付情報を取得する
     // エディットボックスの要素.valueを参照することで、エディットボックスの入力情報を取得できる
-    var newget1 = new Date(daymax[0].value);  //☆　ここは３回覚えていない注意点　valueはエディットボックスに入力した値を取る
+    let newget1 = new Date(daymax[0].value);  //☆　ここは３回覚えていない注意点　valueはエディットボックスに入力した値を取る
     
 
     // 処理順
@@ -213,25 +223,25 @@
     // ➁➀のデータから年を取得する。getfullyear()で取得する。
     // ⓷➁をnewget1の年に反映する。
     // ➀今日の日付情報を取得する。new Date()で取得する
-    var today  =  new Date()
+    let today  =  new Date()
 
     // ➁➀のデータから年を取得する。getfullyear(左上の日付情報)で取得する。
-    var yearget = today.getFullYear()
+    let yearget = today.getFullYear()
 
     // ⓷➁をnewget1(左上の日付情報)の年に反映する。
     newget1.setFullYear(yearget)
 
     // 列内の残りの日付を設定していく
     // daymax[0]は一番左上なので、iの初期値は1
-    for(var i = 1; i < daymax.length; i++)        //ここで+1してるのは1からスタートしてるため
+    for(let i = 1; i < daymax.length; i++)        //ここで+1してるのは1からスタートしてるため
     {
       // 日付情報を次の日に進める
       // newget1.getDate()日付を取得して+1することで次の日に設定できる
       newget1.setDate(newget1.getDate() + 1);   // これはtodayに１足している　　これをそのまま代入するとミリ秒単位ででる
 
       // 設定した日付情報から月と日を取得
-      var month = newget1.getMonth()+1;        // 現在の月の数字を取得＋1は0から開始のため
-      var date = newget1.getDate();           // 現在の日を取得ここは＋１いらない
+      let month = newget1.getMonth()+1;        // 現在の月の数字を取得＋1は0から開始のため
+      let date = newget1.getDate();           // 現在の日を取得ここは＋１いらない
 
       // エディットボックス更新
       // エディットボックスの要素.valueに代入することで、エディットボックスの値を更新できる
@@ -250,10 +260,10 @@
   // 血圧・心拍数データをcookieに保存する
   function cookiecool(){
     // for以下で文字列連結しているため、初期値を空文字にして設定
-    var a = "";                           //なにも表示さしたくないので０ではなく””を代入
+    let a = "";                           //なにも表示さしたくないので０ではなく””を代入
 
     // データの行数だけループする
-    for(var i = 0; i <= 11; i++)
+    for(let i = 0; i <= 11; i++)
     {
       // 各行のデータをコンマで連結する
       // 変数名は違うが、すべての行に同じクラスのデータを配置しているので、
@@ -267,12 +277,12 @@
     document.cookie = `cookie=${a}`;     // 血圧データを入力したときにcookieを保存
   }
 
-    //for文ですべての配列にクラスを入れる
+    //for文ですべての配列にクラスを入れて値を入れてcookieに記憶する
    
-  for(var i = 0;  i < max.length; i++)
+  for(let i = 0; i < max.length; i++)
   {
 
-    //すべての配列に.onchange入った。。？
+    //すべての配列に.onchange入った。
     max[i].onchange = function(){
       maxmax(max,"lastmax");
       cookiecool();
@@ -297,13 +307,11 @@
       cookiecool();
     }
 
-    nightpulse[i].onchange =  function(){
+    nightpulse[i].onchange = function(){
       maxmax(nightpulse,"lastpulse2");
       cookiecool();
     }
 
   }
-
-
 
 })();
